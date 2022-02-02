7752b889ad4b75a7a7251b6e1889d72fe88f75e1
Adelon Medeiros <adelonmedeiros@pop-os.localdomain>
2022-02-01 21:01:15 -0300

    page home completed :tada:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import "../styles/global.scss";
+import styles from "./styles.module.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
